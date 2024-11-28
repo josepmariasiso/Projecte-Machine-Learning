1e37282070f9fd76b3465b5fa734a49e8645934c
Josep Maria Siso <josepmariasiso@users.noreply.github.com>
2024-11-28 20:40:29 +0000

    aaaaaaaaa

--- app.py
+++ app.py
@@ -93,15 +93,33 @@ df.loc[valor_others, 'poutcome'] = 'others'
 df['poutcome']=df['poutcome'].astype('category')
 
 # Apply one-hot encoding to categorical features
-categorical_features = ['job', 'marital', 'contact', 'poutcome']
+#categorical_features = ['job', 'marital', 'contact', 'poutcome']
 #encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')  # sparse=False for streamlit compatibility
-encoded_features = encoder.transform(df[categorical_features])
-encoded_df = pd.DataFrame(encoded_features, columns=encoder.get_feature_names_out(categorical_features))
-encoded_df = encoded_df.drop(['job', 'marital','contact','poutcome'], axis=1)
+#encoded_features = encoder.transform(df[categorical_features])
+#encoded_df = pd.DataFrame(encoded_features, columns=encoder.get_feature_names_out(categorical_features))
+#encoded_df = encoded_df.drop(['job', 'marital','contact','poutcome'], axis=1)
+
+
+new_data = {'job': df['job'], 'marital': df['marital'], 'contact': df['contact'], 'poutcome': df['poutcome']}  
+new_data_df = pd.DataFrame(new_data)
+
+# Transform the categorical features in the new data
+encoded_features = encoder.transform(new_data_df[['job', 'marital', 'contact', 'poutcome']])
+
+# Create a DataFrame from the encoded features
+encoded_features_df = pd.DataFrame(encoded_features.toarray(), 
+                                     columns=encoder.get_feature_names_out(['job', 'marital', 'contact', 'poutcome']))
+
+# Concatenate the encoded features with the rest of the new data
+# Assuming your original new_data_df has other numerical features
+encoded_df= pd.concat([new_data_df, encoded_features_df], axis=1)
+
 
 # Concatenate numerical and encoded features
 input_df = pd.concat([df, encoded_df], axis=1)
-
+input_df  = input_df.drop(['job', 'marital', 'contact', 'poutcome'], axis=1)
+input_df.columns
+input_df.shape
 
 # Scale numerical features
 numerical_features = ['age', 'balance', 'day', 'duration', 'campaign', 'previous']
